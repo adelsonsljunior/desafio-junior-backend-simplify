a5411d6581f4f14ced8d17a8e2daabcb52d0f770
adelsonsljunior <junioradelsonst@gmail.com>
2024-05-02 18:12:20 -0300

    feat: project bootstrap

---
+++ src/test/java/com/adelsonsljunior/desafiojuniorbackendsimplify/DesafioJuniorBackendSimplifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adelsonsljunior.desafiojuniorbackendsimplify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioJuniorBackendSimplifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
