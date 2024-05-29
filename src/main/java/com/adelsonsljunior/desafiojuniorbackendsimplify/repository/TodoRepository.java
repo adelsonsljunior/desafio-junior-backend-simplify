24f337310e4de79bf4d423524a3714c09adfd67e
adelsonsljunior <junioradelsonst@gmail.com>
2024-05-29 09:59:42 -0300

    feat: repository todo

---
+++ src/main/java/com/adelsonsljunior/desafiojuniorbackendsimplify/repository/TodoRepository.java
@@ -0,0 +1,10 @@
+package com.adelsonsljunior.desafiojuniorbackendsimplify.repository;
+
+import com.adelsonsljunior.desafiojuniorbackendsimplify.model.Todo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TodoRepository extends JpaRepository<Todo, Long> {
+
+}
