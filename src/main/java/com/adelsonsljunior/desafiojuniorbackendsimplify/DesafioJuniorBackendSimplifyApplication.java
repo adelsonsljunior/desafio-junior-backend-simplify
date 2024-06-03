03ea1352c77790d74ff21339e6cc43ae1af44dd7
adelsonsljunior <junioradelsonst@gmail.com>
2024-06-02 21:19:04 -0300

    feat: swagger documentation

--- src/main/java/com/adelsonsljunior/desafiojuniorbackendsimplify/DesafioJuniorBackendSimplifyApplication.java
+++ src/main/java/com/adelsonsljunior/desafiojuniorbackendsimplify/DesafioJuniorBackendSimplifyApplication.java
@@ -1,9 +1,12 @@
 package com.adelsonsljunior.desafiojuniorbackendsimplify;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Desafio Júnior Backend Simplify", version = "1", description = "To do management system"))
 public class DesafioJuniorBackendSimplifyApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/adelsonsljunior/desafiojuniorbackendsimplify/controller/TodoController.java
+++ src/main/java/com/adelsonsljunior/desafiojuniorbackendsimplify/controller/TodoController.java
@@ -9,6 +9,12 @@ import com.adelsonsljunior.desafiojuniorbackendsimplify.responses.todo.ListTodoR
 import com.adelsonsljunior.desafiojuniorbackendsimplify.responses.todo.SingleTodoResponse;
 import com.adelsonsljunior.desafiojuniorbackendsimplify.service.TodoService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,7 +27,8 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/todos")
+@RequestMapping(value = "/todos", produces = {"application/json"})
+@Tag(name = "todos")
 public class TodoController {
 
     private final TodoService todoService;
@@ -30,6 +37,11 @@ public class TodoController {
         this.todoService = todoService;
     }
 
+    @Operation(method = "POST", summary = "Create a ToDo")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "ToDo Created Successfully", content = @Content(schema = @Schema(implementation = SingleTodoResponse.class))),
+            @ApiResponse(responseCode = "400", description = "Invalid Parameters", content = @Content(schema = @Schema(implementation = ValidationErrorResponse.class))),
+    })
     @PostMapping
     public ResponseEntity<BaseResponse> create(@RequestBody @Valid TodoCreateDTO data, BindingResult bindingResult) {
 
@@ -41,19 +53,28 @@ public class TodoController {
 
         Todo createdTodo = todoService.create(newTodo);
 
-        BaseResponse response = new SingleTodoResponse("Todo Created Successfully", 201, "Created", createdTodo);
+        BaseResponse response = new SingleTodoResponse("ToDo Created Successfully", 201, "Created", createdTodo);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
+    @Operation(method = "GET", summary = "List all ToDo")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "All ToDo listed successfully")
+    })
     @GetMapping
-    public ResponseEntity<BaseResponse> findAll() {
+    public ResponseEntity<ListTodoResponse> findAll() {
 
         List<Todo> todos = todoService.findAll();
 
-        BaseResponse response = new ListTodoResponse("Todo List", 200, "Ok", todos);
+        ListTodoResponse response = new ListTodoResponse("Todo List", 200, "Ok", todos);
         return ResponseEntity.ok().body(response);
     }
 
+    @Operation(method = "GET", summary = "Find a ToDo")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "ToDo Found Successfully", content = @Content(schema = @Schema(implementation = SingleTodoResponse.class))),
+            @ApiResponse(responseCode = "404", description = "ToDo Not Found"),
+    })
     @GetMapping("/{id}")
     public ResponseEntity<BaseResponse> findById(@PathVariable Long id) {
 
@@ -68,6 +89,12 @@ public class TodoController {
         return ResponseEntity.ok(responseTodo);
     }
 
+    @Operation(method = "PATCH", summary = "Update a Todo")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Todo Updated Successfully", content = @Content(schema = @Schema(implementation = SingleTodoResponse.class))),
+            @ApiResponse(responseCode = "400", description = "Invalid Parameters", content = @Content(schema = @Schema(implementation = ValidationErrorResponse.class))),
+            @ApiResponse(responseCode = "404", description = "Todo Not Found"),
+    })
     @PatchMapping("/{id}")
     public ResponseEntity<BaseResponse> update(@PathVariable Long id, @RequestBody @Valid TodoUpdateDTO data, BindingResult bindingResult) {
 
@@ -95,6 +122,11 @@ public class TodoController {
         return ResponseEntity.ok(response);
     }
 
+    @Operation(method = "DELETE", summary = "Delete a ToDo")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "ToDo deleted successfully", content = @Content),
+            @ApiResponse(responseCode = "404", description = "ToDo Not Found"),
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<BaseResponse> delete(@PathVariable Long id) {
 
